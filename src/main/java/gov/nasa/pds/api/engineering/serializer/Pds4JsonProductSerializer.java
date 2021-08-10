cf5a3929d78ca985964e914cf22c37782d0155b1
Al Niessner <Al.Niessner@xxx.xxx>
2021-08-10 12:35:50 -0700

    xml never to be see again

--- src/main/java/gov/nasa/pds/api/engineering/serializer/Pds4JsonProductSerializer.java
+++ src/main/java/gov/nasa/pds/api/engineering/serializer/Pds4JsonProductSerializer.java
@@ -1,32 +1,16 @@
 package gov.nasa.pds.api.engineering.serializer;
 
-import java.io.IOException;
-import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
-import javax.xml.stream.XMLOutputFactory;
-import javax.xml.stream.XMLStreamWriter;
-import javax.xml.transform.Result;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpInputMessage;
-import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.util.StreamUtils;
 
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import gov.nasa.pds.api.model.xml.ProductWithXmlLabel;
-import gov.nasa.pds.model.Product;
-
-
 
 public class Pds4JsonProductSerializer extends MappingJackson2HttpMessageConverter {
 	
@@ -38,13 +22,12 @@ public class Pds4JsonProductSerializer extends MappingJackson2HttpMessageConvert
 		
 		List<MediaType> supportMediaTypes = new ArrayList<MediaType>();
 		supportMediaTypes.add(MediaType.APPLICATION_JSON);
+		// JILT: need to remove the media types below if http/xml is once again desired
+		supportMediaTypes.addAll(Arrays.asList(MediaType.APPLICATION_XML, MediaType.TEXT_XML, new MediaType("application", "*+xml")));
 		this.setSupportedMediaTypes(supportMediaTypes);
-		
+
 		ObjectMapper mapper = new ObjectMapper();
 	    mapper.setSerializationInclusion(Include.NON_NULL);
-	    this.setObjectMapper(mapper);
-	     
+	    this.setObjectMapper(mapper);    
 	}
-	
-
 }
