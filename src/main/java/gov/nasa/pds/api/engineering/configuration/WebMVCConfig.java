acc15338a07816ab4f69120bd90cf4799a4ddd90
Al Niessner <Al.Niessner@xxx.xxx>
2021-12-20 12:13:39 -0800

    finally, endpoint lookup now ignores accept

--- src/main/java/gov/nasa/pds/api/engineering/configuration/WebMVCConfig.java
+++ src/main/java/gov/nasa/pds/api/engineering/configuration/WebMVCConfig.java
@@ -6,7 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -28,62 +27,59 @@ import gov.nasa.pds.api.engineering.serializer.XmlProductSerializer;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = { "gov.nasa.pds.api.engineering.configuration ",  "gov.nasa.pds.api.engineering.controllers", "gov.nasa.pds.api.engineering.elasticsearch"})
-public class WebMVCConfig implements WebMvcConfigurer {
-    
-    private static final Logger log = LoggerFactory.getLogger(WebMVCConfig.class);
-    
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+public class WebMVCConfig implements WebMvcConfigurer
+{   
+	private static final Logger log = LoggerFactory.getLogger(WebMVCConfig.class);
+ 
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry)
+	{
+		registry.addResourceHandler("swagger-ui.html")
+		.addResourceLocations("classpath:/META-INF/resources/");
 
-           registry.addResourceHandler("swagger-ui.html")
-                    .addResourceLocations("classpath:/META-INF/resources/");
-
-            registry.addResourceHandler("/webjars/**")
-                    .addResourceLocations("classpath:/META-INF/resources/webjars/");
+		registry.addResourceHandler("/webjars/**")
+		.addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
-    
-    
-     @Override
-       public void configurePathMatch(PathMatchConfigurer configurer) {
-         // this is important to avoid that parameters (e.g lidvid) are truncated after .
-           configurer.setUseSuffixPatternMatch(false);
-       }
-    
-    
-  /**
-   * Setup a simple strategy: use all the defaults and return XML by default when not sure. 
- */
-  @SuppressWarnings("deprecation")
-public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-    configurer.defaultContentType(MediaType.APPLICATION_JSON);
-    
-    /*
-    // For path content negociation , .json. .xml ...
-    // that does not work on its own, I guess I also need to update the swagger definition
-    configurer.favorParameter(false).
-    ignoreAcceptHeader(false).
-    defaultContentType(MediaType.APPLICATION_JSON).
-    mediaType("xml", MediaType.APPLICATION_XML).
-    mediaType("json", MediaType.APPLICATION_JSON);
-    */
-  }
-  
-  
-  @Override
-  public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-   
-      WebMVCConfig.log.info("Number of converters available " + Integer.toString(converters.size()));
-      converters.add(new CsvPluralSerializer());
-      converters.add(new CsvSingularSerializer());
-      converters.add(new JsonPluralSerializer());
-      converters.add(new JsonSingularSerializer());
-      converters.add(new JsonProductSerializer());
-      converters.add(new Pds4JsonProductSerializer());
-      converters.add(new Pds4JsonProductsSerializer());
-      converters.add(new Pds4XmlProductSerializer());
-      converters.add(new XmlProductSerializer()); // Product class, application/xml
-      //converters.add(new Jaxb2RootElementHttpMessageConverter()); // other classes, application/xml
-  }
 
-  
+	@Override
+	@SuppressWarnings("deprecation")
+	public void configurePathMatch(PathMatchConfigurer configurer)
+	{
+		// this is important to avoid that parameters (e.g lidvid) are truncated after .
+		configurer.setUseSuffixPatternMatch(false);
+	}
+
+	/**
+	 * Setup a simple strategy: use all the defaults and return XML by default when not sure. 
+	 */
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer)
+	{
+		log.error ("***********           ignoring accept header");
+		configurer.ignoreAcceptHeader(true);
+		configurer.favorParameter(true);
+		/*
+    	// For path content negociation , .json. .xml ...
+    	// that does not work on its own, I guess I also need to update the swagger definition
+    	configurer.favorParameter(false).
+    	defaultContentType(MediaType.APPLICATION_JSON).
+    	mediaType("xml", MediaType.APPLICATION_XML).
+    	mediaType("json", MediaType.APPLICATION_JSON);
+		 */
+	}
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters)
+	{
+		WebMVCConfig.log.info("Number of converters available " + Integer.toString(converters.size()));
+		converters.add(new CsvPluralSerializer());
+		converters.add(new CsvSingularSerializer());
+		converters.add(new JsonPluralSerializer());
+		converters.add(new JsonSingularSerializer());
+		converters.add(new JsonProductSerializer());
+		converters.add(new Pds4JsonProductSerializer());
+		converters.add(new Pds4JsonProductsSerializer());
+		converters.add(new Pds4XmlProductSerializer());
+		converters.add(new XmlProductSerializer()); // Product class, application/xml
+		//converters.add(new Jaxb2RootElementHttpMessageConverter()); // other classes, application/xml
+	}
 }
